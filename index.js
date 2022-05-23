acd47fbb84a55bbd70dde7d4021f70195917a1b9
kibria khandaker <46866083+kibria-khandaker@users.noreply.github.com>
2022-05-24 00:09:45 +0600

    Tools Add and Booking  API created

--- index.js
+++ index.js
@@ -20,12 +20,46 @@ async function run() {
         //--------
         await client.connect();
         const toolsCollection = client.db("toolsData").collection("tools");
+        const bookingCollection = client.db("toolsData").collection("bookings");
+ 
 
         // Get Data From MDB
         app.get('/tools', async (req, res) => {
             const myTools = await toolsCollection.find({}).toArray();
             res.send(myTools)
         })
+
+ 
+
+
+        //  GET booking product from MDB
+        // http://localhost:5000/booking?patient=kibriakhandaker66@gmail.com
+        app.get('/booking', async (req, res) => {
+            const bookUserEmail = req.query.bookUserEmail;
+            
+            const authorization = req.headers.authorization;
+            console.log('Auth Header', authorization);
+
+            const query = { bookUserEmail: bookUserEmail }
+            const userBookings = await bookingCollection.find(query).toArray()
+            res.send(userBookings)
+        })
+
+        // booking post or sent data to MDB 
+        app.post('/booking', async (req, res) => {
+            const booking = req.body;
+            const query = {
+                bookToolsId: booking.bookToolsId,
+                bookUserEmail: booking.bookUserEmail
+            };
+            const exists = await bookingCollection.findOne(query);
+            if (exists) {
+                return res.send({ success: false, booking: exists })
+            }
+            const myBooking = await bookingCollection.insertOne(booking);
+            return res.send({ success: true, myBooking });
+        })
+
 
     } finally { }
 }
