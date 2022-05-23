2e13d061756f5c52f7ab98a55dfcd01e6a80e211
kibria khandaker <46866083+kibria-khandaker@users.noreply.github.com>
2022-05-24 03:33:20 +0600

    Admin API  amd Admin Email Filter API Created

--- index.js
+++ index.js
@@ -45,20 +45,27 @@ async function run() {
             res.send(myTools)
         })
 
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await userCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin })
+        })
+
         app.put('/user/admin/:email', verifyJWT, async (req, res) => {
-            
+
             const email = req.params.email;
             const requester = req.decoded.email;
             const requesterAccount = await userCollection.findOne({ email: requester })
             if (requesterAccount.role === 'admin') {
-            const filter = { email: email };
-            const updateDoc = {
-                $set: { role: 'admin' },
-            };
-            const result = await userCollection.updateOne(filter, updateDoc);
-            res.send(result);
+                const filter = { email: email };
+                const updateDoc = {
+                    $set: { role: 'admin' },
+                };
+                const result = await userCollection.updateOne(filter, updateDoc);
+                res.send(result);
             } else {
-              res.status(403).send({ message: 'Forbidden to access' })
+                res.status(403).send({ message: 'Forbidden to access' })
             }
 
         })
