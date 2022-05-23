765b6edd51594f8022d7573c475b2a86531c325f
kibria khandaker <46866083+kibria-khandaker@users.noreply.github.com>
2022-05-23 23:56:18 +0600

    MongoDB conection Done

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -10,7 +11,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+//-------------------------------------------------------------
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lytri.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+
+    } finally { }
+}
+run().catch(console.dir)
+//-------------------------------------------------------------
 
 app.get('/', (req, res) => {
     res.send('Your -Construction Tools Manufacturer- Server is Running')
